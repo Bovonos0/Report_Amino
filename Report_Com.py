029cf1cefcc8e138ee9c42206c411c0a99aeb246
Bovonos0 <71797403+Bovonos0@users.noreply.github.com>
2020-11-22 05:48:24 +0300

    Add files via upload
    pkg update && pkg upgrade -y
    pkg install git -y
    pkg install python -y
    pkg install python2 -y
    pip3 install Amino.py
    git clone https://github.com/Bovonos0/Report_Amino
    cd Report_Amino
    python3 Report_Com.py

---
+++ Report_Com.py
@@ -0,0 +1,25 @@
+import amino
+import os
+red="\033[0;31m"
+client=amino.Client()
+email=email("Type Your Email: ")
+password=input("Type Your Password: ")
+try:
+	client.login(email=email ,password=password)
+	print ("Done")
+except:
+	print ("Your email or password is incorrect")
+	os._exit()
+comlink=input("Type Community Link: ")
+com=client.get_from_code(comlink)
+comId=com.path[1:com.path.index('/')]
+subclient=amino.SubClient(comId=comId ,profile=client.profile)
+
+reason=input("Type Your Reason: ")
+while True:
+	try:
+		subclient.flag_community(comId=comId, reason=reason ,flagType=3 , isGuest=False)
+		print(red + "Report Send")
+	except:
+		pass
+		print ("Fail Sending Report")
